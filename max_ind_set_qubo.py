8579e8ec2a37acc5f4010fdd88996f2c74d75106
Steve Hart <stevenrhart@gmail.com>
2020-10-06 21:18:59 +0000

    updated based on Alex's comments

--- max_ind_set_qubo.py
+++ max_ind_set_qubo.py
@@ -27,11 +27,6 @@ import matplotlib
 matplotlib.use("agg")
 import matplotlib.pyplot as plt
 
-def get_token():
-    '''Return your personal access token'''
-    
-    return token
-
 def create_graph(edges):
     """Returns a graph based on the specified list of edges.
 
@@ -57,60 +52,47 @@ def get_qubo(nodes, edges):
     
     # Create QUBO
     Q = {}
-    for i in range(len(nodes)):
-        for j in range(len(nodes)):
-            if j > i and (i, j) in edges:
-                Q[(i, j)] = gamma
-            elif j < i:
-                Q[(i, j)] = 0
-            else: 
-                Q[(i, i)] = -1
-    
-    # Print qubo in matrix form
-    # for i in range(len(nodes)):
-    #     my_line = ''
-    #     for j in range(len(nodes)):
-    #         if (i,j) in Q:
-    #             my_line += '\t' + str(Q[(i,j)])
-    #         else:
-    #             my_line += '\t' + str(0)
-    #     print(my_line)
+    for i in G.nodes:
+        Q[(i, i)] = -1
+    for i, j in G.edges:
+        Q[(i, j)] = gamma
 
     return Q
 
-def run_on_qpu(Q, sampler, chainstrength, numruns):
+def run_on_qpu(Q, sampler, chainstrength, num_reads):
     """Runs the QUBO problem Q on the sampler provided.
 
     Args:
         Q(dict): a representation of a QUBO
         sampler(dimod.Sampler): a sampler that uses the QPU
     """
-    sample_set = sampler.sample_qubo(Q, chain_strength=chainstrength, num_reads=numruns)
+    sample_set = sampler.sample_qubo(Q, chain_strength=chainstrength, num_reads=num_reads)
 
     return sample_set
 
 ## ------- Main program -------
 if __name__ == "__main__":
 
+    # # Test Graph 1 
     # nodes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    # edges = [(0, 1), (0, 2), (1, 3), (2, 3), (3, 4), (4, 5), (4, 6), (5, 6), (5, 7), (6, 8), (7, 8)]  
+    # edges = [(0, 1), (0, 2), (1, 3), (2, 3), (3, 4), (4, 5), (4, 6), (5, 6), (5, 7), (6, 8), (7, 8)]
+
+    # Test Graph 2
     nodes = [0, 1, 2, 3, 4, 5, 6, 7]
-    edges = [(0, 1), (0, 2), (1, 3), (2, 3), (3, 4), (3, 5), (3, 6), (4, 7), (6, 7)]  
+    edges = [(0, 1), (0, 2), (1, 3), (2, 3), (3, 4), (3, 5), (3, 6), (4, 7), (6, 7)]
     
-    token = get_token()
     G = create_graph(edges)
     Q = get_qubo(nodes, edges)
 
     chainstrength = 1 # update
-    numruns = 10 # update
+    num_reads = 10 # update
 
     # Define the sampler and run the problem
-    sampler = EmbeddingComposite(DWaveSampler(endpoint='https://cloud.dwavesys.com/sapi/', token=token, solver={'qpu': True}))
-    sample_set = run_on_qpu(Q, sampler, chainstrength, numruns)
+    sampler = EmbeddingComposite(DWaveSampler(endpoint='https://cloud.dwavesys.com/sapi/', solver={'qpu': True}))
+    sample_set = run_on_qpu(Q, sampler, chainstrength, num_reads)
 
     # Print the solution
     print('Maximum independent set size found is', sum(sample_set.first.sample.values()))
-    # print(sample_set.first.sample)
     
     # Generate list of nodes in final solution
     binaryResult = list(sample_set.first.sample.values())
